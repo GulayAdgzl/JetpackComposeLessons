7a81bbdb0d2c264eefc69b632fe46ecdde3ff064
gulay.adiguzel <gulay.adiguzel@peakup.org>
2025-01-07 21:51:16 +0300

    orizontalAlignment: JdkConstants.HorizontalAlignment = HorizontalAlignment

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import org.intellij.lang.annotations.JdkConstants
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,24 +27,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
-                    Greeting(
-                        modifier = Modifier.padding(paddingValues)
-                    )
+                Scaffold() {
+
                 }
+                Greeting(
+
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Text(
-        text = "DENEME",
-        color = Color.Red,
-        modifier = modifier,
-        fontStyle= FontStyle.Italic
-    )
+fun Greeting(modifier: Modifier = Modifier.padding(25 .dp)
+             ,orizontalAlignment: JdkConstants.HorizontalAlignment = HorizontalAlignment) {
+
+    Column {
+
+
+        Text(
+            text = "DENEME",
+            color = Color.Red,
+            modifier = modifier.padding(25 .dp),
+            fontStyle= FontStyle.Italic
+        )
+
+        Text(
+            text = "DENEME",
+            color = Color.Red,
+            modifier = modifier.padding(25 .dp),
+            fontStyle= FontStyle.Italic
+        )
+    }
 }
 
 @Preview(showBackground = true)
